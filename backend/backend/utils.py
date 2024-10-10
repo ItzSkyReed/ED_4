c4c65c5a5e02ca043c319e34ae2df90af1aeb625
ItzSkyReed <itzskyreed@gmail.com>
2024-10-10 09:11:25 +0500

    проверка на создание data.json

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -1,9 +1,14 @@
+import os
 from datetime import datetime
 
 import json
 
 
 def write_to_data(json_data: dict):
+    if not os.path.exists('data.json'):
+        with open('data.json', 'w', encoding='utf-8') as file:
+            json.dump({}, file, ensure_ascii=False, indent=4)
+
     # Читаем существующие данные из файла
     with open('data.json', encoding='utf-8') as file:
         data: dict = json.load(file)
@@ -19,4 +24,3 @@ def write_to_data(json_data: dict):
 def get_data_json() -> str:
     with open(r'data.json', encoding='utf-8') as file:
         return file.read()
-
